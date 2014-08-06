3a5f0c4ab73a6fa689da522363ca59493f18b4f8
Zhang hao <jsyczhanghao@sina.com>
2014-08-06 22:26:30 +0800

    Update template.js

--- mod/template/template.js
+++ mod/template/template.js
@@ -4,16 +4,18 @@ module.exports = {
 	},
 
 	parse: function(content, data){
+        	var tmp = "with(d){r.push('" 
+	                + 
+	                content.replace(/(')|([\r\n]+)|<%(=?)(.*?)%>/g, function(_0, _1, _2, _3, _4){
+	                    return _1 ? "\\'" : _2 ? "" : _3 ? "' + (" + _4 + ") + '" : "'); \r\n" + _4 + "; \r\nr.push('";
+	                }) 
+	                + 
+	                "');}return r.join('');";
+	                
 		try{
-			return (new Function('d', 'r', 'c', "with(d){r.push('" 
-				+ 
-				content.replace(/(')|([\r\n]+)|<%(=?)(.*?)%>/g, function(_0, _1, _2, _3, _4){
-					return _1 ? "\\'" : _2 ? "" : _3 ? "' + (" + _4 + ") + '" : "'); " + _4 + " r.push('";
-				}) 
-				+ 
-				"');}return r.join('');"))(data, []);
+			return (new Function('d', 'r', 'c', tmp))(data, []);
 		}catch(e){
-			console && console.log(content);
+			console && console.log(tmp);
 			throw new Error(content);
 		}
 	}
